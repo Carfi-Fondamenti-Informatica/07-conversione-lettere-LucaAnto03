6659fa4dd37b6728fe7fad384165dc601674340b
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-07 12:25:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+int main () {
+    char l='a';
+    cin >> l;
+    if ( verifica (l)) {
+        cout << l;
+    } else  {
+     cout << "errore" << endl;
+     }
+    return 0;
 }
+
