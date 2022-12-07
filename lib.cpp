2fde51d63b63e590b7431e5ea9d668f6e9dc2de4
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-07 12:23:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+bool verifica (char &l){
+    if ((l>=65)&&(l<=90)) {
+        l=l+32;
+        return true;
+    } else if ((l>=97)&&(l<=122)) {
+        l=l-32;
+        return true;
+    } else {
+        return false;
+    }
+}
